b1daa9a695978832c99ac2368299cbef29c8119b
dov_dev <dov1981@gmail.com>
2024-11-29 17:52:13 +0100

    Actualización home

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ from home import main
 
 # Obtener la contraseña desde la variable de entorno
 PASSWORD = os.getenv("APP_PASSWORD_DOV", "defaultPassword")  # "defaultPassword" es el valor por defecto si no está configurada la variable
-PRUEBA = os.getenv("APP_PRUEBA_DOV", "defaultPassword")
 
-print("Contraseña desde la variable de entorno:", PRUEBA)
- 
 def main(page: ft.Page):
     page.title = "Aplicación Protegida"
 
     # Función de autenticación
     def verificar_password(e):
         if password_input.value == PASSWORD:
-            page.clean()  # Limpia la página
-            main(page)  # Llama nuevamente a la función principal para mostrar el contenido
+            login_form.visible = False  # Ocultar el formulario de login
+            page.add(main_content)  # Mostrar el contenido principal
+            page.update()  # Actualizar la página
         else:
             error_message.value = "Contraseña incorrecta"
             page.update()
@@ -25,20 +23,24 @@ def main(page: ft.Page):
     error_message = ft.Text(value="", color="red")
     login_button = ft.ElevatedButton(text="Acceder", on_click=verificar_password)
 
-    # Añadir los controles a la pantalla inicial
-    page.add(
-        ft.Column(
-            [
-                ft.Text("Acceso Restringido", size=24, weight="bold"),
-                password_input,
-                error_message,
-                login_button,
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            spacing=20,
-        )
+    # Contenedor para el formulario de login
+    login_form = ft.Column(
+        [
+            ft.Text("Acceso Restringido", size=24, weight="bold"),
+            password_input,
+            error_message,
+            login_button,
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        spacing=20,
     )
+
+    # Contenido principal que se muestra después del login (puedes modificar este contenido)
+    main_content = ft.Text("Contenido de la aplicación", size=24)
+
+    # Añadir el formulario de login a la página
+    page.add(login_form)
 
 # Ejecutar el servidor web sin especificar `view`
 ft.app(target=main)
